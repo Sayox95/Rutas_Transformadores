db7e896bf15c55fe535de5e6420560d2127b5911
Formularios UTCD <sayoxlive@gmail.com>
2026-04-30 12:05:36 -0600

    Update status.js

--- functions/api/osrm/batch/status.js
+++ functions/api/osrm/batch/status.js
@@ -4,7 +4,7 @@ function json(data, status = 200) {
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Methods": "GET, OPTIONS", 
       "Access-Control-Allow-Headers": "Content-Type",
     },
   });
